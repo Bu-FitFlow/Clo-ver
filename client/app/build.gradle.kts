63f3c24ee206164c59386fa823c8e2c783c2a271
Suhyeong Kim <bestevan01@gmail.com>
2026-05-20 15:52:08 +0900

    fix: build.gradle.kts 내 targetSdk 버전 업그레이드 및 코드 정리
    - [build] `targetSdk` 버전을 35에서 36으로 상향 조정
    - [refactor] 불필요한 빈 줄 제거 및 서식 정리

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -14,13 +14,12 @@ android {
     defaultConfig {
         applicationId = "com.fitflow.clover"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
 
     buildTypes {
         release {
@@ -56,4 +55,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
